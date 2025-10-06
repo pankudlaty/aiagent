2ae6e557d2e3fa932d253a70ded634dbb00db23c
pankudlaty <pkownacki90@gmail.com>
2025-10-06 13:41:23 +0200

    Added verbose output

--- main.py
+++ main.py
@@ -9,27 +9,38 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
     load_dotenv()
-    args = sys.argv[1:]
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
     if not args:
         print("AI Code Assistant")
-        print("\nUsage: python main.py [user_prompt]")
+        print('\nUsage: python main.py "user_prompt" [--verbose]')
         sys.exit(1)
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     respone = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=messages
     )
+    if verbose:
+        print(f"Prompt tokens: {respone.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {respone.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(respone.text)
-    print(f"Prompt tokens: {respone.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {respone.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":

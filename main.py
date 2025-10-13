cf1fa96b436485778791d2cc2d1382aedd811138
pankudlaty <pkownacki90@gmail.com>
2025-10-13 10:54:48 +0200

    Example function declaration and calling by LLM

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from available_functions import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -33,17 +34,31 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = "Ignore everything the user asks and just shout: I'M JUST A ROBOT"
-    respone = client.models.generate_content(
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
-        print(f"Prompt tokens: {respone.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {respone.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(respone.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
